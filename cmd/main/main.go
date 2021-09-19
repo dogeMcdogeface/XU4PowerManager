5320b272ebb49ac4125d4227e620eb5dfbe5e6b8
dogeMcdogeface <nmanta@protonmail.com>
2021-09-19 14:20:00 +0200

    ftp test

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"XU4PowerManager/internal"
+	"bufio"
 	"fmt"
 	_ "github.com/spf13/viper"
+	"log"
+	"os"
 	"time"
 )
 
 func main() {
 	fmt.Println("Starting XU4 Power Manager ver." + internal.Ver)
 
+	file, err := os.Open(internal.Thermal0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
 	for true {
+		fmt.Println(scanner.Text())
 
 		fmt.Println("Update")
 		time.Sleep(internal.UpdateTime)
--- internal/defaultConfiguration.go
+++ internal/defaultConfiguration.go
@@ -4,7 +4,7 @@ import "time"
 
 var Ver = "12.3"
 
-var thermal0 = "/sys/devices/virtual/thermal/thermal_zone0/temp"
+var Thermal0 = "/sys/devices/virtual/thermal/thermal_zone0/temp"
 
 var UpdateFrequency = 1.
 var UpdateTime = time.Second / time.Duration(UpdateFrequency)
