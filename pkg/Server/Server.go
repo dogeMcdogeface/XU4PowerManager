59b55aaee2ce7bf62e4dac46342accba0698920e
dogeMcdogeface <nmanta@protonmail.com>
2021-09-19 15:01:41 +0200

    ftp test

--- pkg/Server/Server.go
+++ pkg/Server/Server.go
@@ -11,6 +11,7 @@ var Status = "off"
 var Temp = "0"
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Server says " + Temp)
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 	fmt.Fprintf(w, "Temp is  %s!", Temp)
 
