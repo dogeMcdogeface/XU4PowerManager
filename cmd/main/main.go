8d3ef8a997ce1dd17e5a3371b4909ad1b7d9cf86
dogeMcdogeface <nmanta@protonmail.com>
2021-09-19 14:17:17 +0200

    ftp test

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,8 +4,15 @@ import (
 	"XU4PowerManager/internal"
 	"fmt"
 	_ "github.com/spf13/viper"
+	"time"
 )
 
 func main() {
-	fmt.Println("hello world 6 " + internal.Ver)
+	fmt.Println("Starting XU4 Power Manager ver." + internal.Ver)
+
+	for true {
+
+		fmt.Println("Update")
+		time.Sleep(internal.UpdateTime)
+	}
 }
