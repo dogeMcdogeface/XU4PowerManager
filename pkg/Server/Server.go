e0e3ea88ac1aa3f68f9fff8b889ffebfb12d45b3
dogeMcdogeface <nmanta@protonmail.com>
2021-09-20 09:22:32 +0200

    ftp test

--- pkg/Server/Server.go
+++ pkg/Server/Server.go
@@ -14,10 +14,15 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Server says " + Temp)
 	fmt.Fprintf(w, Page, Temp)
 	/*fmt.Fprintf(w, "Temp is  %s!", Temp)*/
-
+}
+func handler2(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Server says " + Temp)
+	fmt.Fprintf(w, "asdads %s", Temp)
+	/*fmt.Fprintf(w, "Temp is  %s!", Temp)*/
 }
 
 func Start() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/a", handler2)
 	log.Fatal(http.ListenAndServe(":9012", nil))
 }
