a89f1c24dcd7ec5c0e3f2d306c2c91e328716d28
dogeMcdogeface <nmanta@protonmail.com>
2021-09-20 10:14:08 +0200

    added hardware polling package

--- pkg/HWReader/HWReader.go
+++ pkg/HWReader/HWReader.go
@@ -1,9 +1,9 @@
 package HWReader
 
 import (
-	"XU4PowerManager/pkg/Server"
 	"fmt"
 	"io/ioutil"
+	"strconv"
 	"time"
 )
 
@@ -11,15 +11,25 @@ var enabled = false
 var UpdateFrequency = 10.
 var UpdateTime = time.Second / time.Duration(UpdateFrequency)
 
-var Thermal0 = "/sys/devices/virtual/thermal/thermal_zone0/temp"
+var Sensors = map[string]string{
+	"Thermal0": "/sys/devices/virtual/thermal/thermal_zone0/temp",
+}
+
+var LastRead = map[string]interface{}{}
 
 func Start() {
 	enabled = true
 
 	for enabled == true {
-		content, _ := ioutil.ReadFile(Thermal0)
-		fmt.Println(string(content))
-		Server.Temp = string(content)
+
+		for key, value := range Sensors {
+			//fmt.Println("Key:", key, "Value:", value)
+			LastRead[key] = readSensor(value)
+		}
+		fmt.Println(LastRead)
+
+		/*fmt.Println(string(content))
+		Server.Temp = string(content)*/
 
 		fmt.Println("Update")
 		time.Sleep(UpdateTime)
@@ -28,4 +38,11 @@ func Start() {
 
 func Stop() {
 	enabled = false
+}
+
+func readSensor(path string) int {
+	content, _ := ioutil.ReadFile(path)
+	value, _ := strconv.Atoi(string(content))
+
+	return value
 }
