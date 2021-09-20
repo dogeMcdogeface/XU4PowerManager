18992ff95415feaf4692e52f8b1488b77889cb57
dogeMcdogeface <nmanta@protonmail.com>
2021-09-20 09:28:31 +0200

    ftp test

--- pkg/Server/Server.go
+++ pkg/Server/Server.go
@@ -7,22 +7,34 @@ import (
 )
 
 var Status = "off"
-
 var Temp = "0"
 
-func handler(w http.ResponseWriter, r *http.Request) {
+/*var pages = map[string]int{
+	"rsc": 3711,
+	"r":   2138,
+	"gri": 1908,
+}*/
+
+func handlerDefault(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Server says " + Temp)
 	fmt.Fprintf(w, Page, Temp)
+}
+
+func handlerLast(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Server says " + Temp)
+	fmt.Fprintf(w, "last %s", Temp)
 	/*fmt.Fprintf(w, "Temp is  %s!", Temp)*/
 }
-func handler2(w http.ResponseWriter, r *http.Request) {
+
+func handlerHistory(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Server says " + Temp)
-	fmt.Fprintf(w, "asdads %s", Temp)
+	fmt.Fprintf(w, "history %s", Temp)
 	/*fmt.Fprintf(w, "Temp is  %s!", Temp)*/
 }
 
 func Start() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/a", handler2)
+	http.HandleFunc("/", handlerDefault)
+	http.HandleFunc("/last", handlerLast)
+	http.HandleFunc("/history", handlerHistory)
 	log.Fatal(http.ListenAndServe(":9012", nil))
 }
