e647cc0130a7853faa6c309a50a8537348d7db01
dogeMcdogeface <nmanta@protonmail.com>
2021-09-26 21:50:39 +0200

    Adding hardware control

--- pkg/HWWriter/HWWriter.go
+++ pkg/HWWriter/HWWriter.go
@@ -1,26 +1,17 @@
 package HWWriter
 
 import (
-	"bytes"
-	"fmt"
 	"log"
 	"os/exec"
-	"strings"
 )
 
 func Echo(path, txt string) {
-	cmd := exec.Command("tr", "a-z", "A-Z")
 
-	cmd.Stdin = strings.NewReader("and old falcon")
-
-	var out bytes.Buffer
-	cmd.Stdout = &out
+	cmd := exec.Command("echo ass > t.txt")
 
 	err := cmd.Run()
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Printf("translated phrase: %q\n", out.String())
 }
