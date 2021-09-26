d00486f759ee68af63313c4fcd05f7924df2ccc5
dogeMcdogeface <nmanta@protonmail.com>
2021-09-26 21:54:01 +0200

    Adding hardware control

--- pkg/HWWriter/HWWriter.go
+++ pkg/HWWriter/HWWriter.go
@@ -1,17 +1,31 @@
 package HWWriter
 
 import (
-	"log"
+	"fmt"
+	"os"
 	"os/exec"
 )
 
 func Echo(path, txt string) {
 
-	cmd := exec.Command("echo ass > t.txt")
+	//cmd := exec.Command("echo ass > t.txt")
+	//err := cmd.Run()
 
-	err := cmd.Run()
+	call("echo ass > t.txt")
 
+}
+
+func call(command string) {
+	cmd := exec.Command("sudo", "bash", "-c", command)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Start()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+	}
+	err1 := cmd.Wait()
+	if err1 != nil {
+		fmt.Println(err1)
 	}
 }
